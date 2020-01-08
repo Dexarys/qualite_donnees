58349c27c9fa77873ec9712ad188c5aef2f446a0
l.arif@laposte.net <l.arif@laposte.net>
2020-01-08 09:56:10 +0100

    QA

--- exec.py
+++ exec.py
@@ -2,10 +2,7 @@ import matplotlib.pyplot as plt
 import xlrd
 import math
 import statistics as sta
-
-File = xlrd.open_workbook('Climat.xlsx')
-sheet = File.sheet_by_name('Feuil2')
-
+from matplotlib.widgets import Slider
 
 def num(s):
     try:
@@ -40,8 +37,8 @@ def getMax(col_array):
 
 
 def print_month_info(col, data):
-    month_lst = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-                 'August', 'September', 'October', 'November', 'December']
+    month_lst = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet',
+                 'Août', 'Septembre', 'Octobre', 'Novembre', 'Decembre']
     return month_lst[col]+" : "+str(data)
 
 
@@ -51,48 +48,53 @@ def get_ecart_type(moyenne, col_array):
         if value == "":
             break
         variance = variance + ((num(value) - moyenne)*(num(value) - moyenne))
-
     return round(math.sqrt(variance/len(col_array)), 1)
 
 
-year_min_values = []
-year_max_values = []
-month_value = []
-years_values = []
-
-for i in range(sheet.ncols):
-    moy = getMoyenne(sheet.col_values(i))
-    min = getMin(sheet.col_values(i))
-    max = getMax(sheet.col_values(i))
-    year_min_values.append(min)
-    year_max_values.append(max)
-    for h in sheet.col_values(i):
-        if h == "":
-            break
-        month_value.append(num(h))
-    print("====================")
-    print("Moyenne: " + print_month_info(i, moy))
-    print("Minimum: " + print_month_info(i, min))
-    print("Maximum: " + print_month_info(i, max))
-    print("Ecart type: " + str(get_ecart_type(moy, sheet.col_values(i))))
-    print("--------------------------------------")
-    plt.figure(print_month_info(i, ""))
-    plt.plot(month_value)
+def exec_exercice_1():
+    File = xlrd.open_workbook('Climat.xlsx')
+    sheet = File.sheet_by_name('Feuil2')
+    year_min_values = []
+    year_max_values = []
     month_value = []
-    for j in sheet.col_values(i):
-        if j == "":
-            break
-        years_values.append(num(j))
+    years_values = []
+    for i in range(sheet.ncols):
+        moy = getMoyenne(sheet.col_values(i))
+        min = getMin(sheet.col_values(i))
+        max = getMax(sheet.col_values(i))
+        year_min_values.append(min)
+        year_max_values.append(max)
+        for h in sheet.col_values(i):
+            if h == "":
+                break
+            month_value.append(num(h))
+        print("====================")
+        print("Moyenne: " + print_month_info(i, moy))
+        print("Minimum: " + print_month_info(i, min))
+        print("Maximum: " + print_month_info(i, max))
+        print("Ecart type: " + str(get_ecart_type(moy, sheet.col_values(i))))
+        print("--------------------------------------")
+        plt.figure(print_month_info(i, ""))
+        plt.plot(month_value)
+        axes = plt.gca()
+        Slider(axes, "toto", 1, 30, valinit=1)
+        month_value = []
+        for j in sheet.col_values(i):
+            if j == "":
+                break
+            years_values.append(num(j))
 
-print("====================")
-print("Minimum for year : " + str(getMin(year_min_values)))
-print("Maximum for year : " + str(getMax(year_max_values)))
-plt.figure("Years")
-plt.plot(years_values)
-plt.ylabel("Temperature °C")
-plt.xlabel("Jour")
-plt.show()
+    print("====================")
+    print("Minimum for year : " + str(getMin(year_min_values)))
+    print("Maximum for year : " + str(getMax(year_max_values)))
+    plt.figure("Years")
+    plt.plot(years_values)
+    plt.ylabel("Temperature °C")
+    plt.xlabel("Jour")
+    plt.show()
 
+
+exec_exercice_1()
 
 
 
