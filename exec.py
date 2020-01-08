44eef4043b3a0d2873dca2efe94909b87f60ba80
Matthieu F <fallourd.matthieu@gmail.com>
2020-01-08 13:29:08 +0100

    fix slider

--- exec.py
+++ exec.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
+from matplotlib.widgets import Slider, Button, RadioButtons
 import xlrd
 import math
 import statistics as sta
+import numpy as np
 
 
 def num(s):
@@ -90,10 +91,52 @@ def exec_exercice_1():
     print("====================")
     print("Minimum for year : " + str(getMin(year_min_values)))
     print("Maximum for year : " + str(getMax(year_max_values)))
-    plt.figure("Years")
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(left=0.25, bottom=0.25)
+    min0 = 0
+    max0 = 10
+
     plt.plot(years_values)
-    plt.ylabel("Temperature °C")
-    plt.xlabel("Jour")
+    # most examples here return something iterable
+
+    plt.xlim([1, 31])  # initial limits
+
+    axmin = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+
+    smin = Slider(axmin, 'Months', 1, 12, valinit=min0, valstep=1)
+
+    def update(val):
+        val = num(val)
+        if val == 1:
+            plt.xlim([1, 31])
+        elif val == 2:
+            plt.xlim([32, 59])
+        elif val == 3:
+            plt.xlim([60, 90])
+        elif val == 4:
+            plt.xlim([91, 120])
+        elif val == 5:
+            plt.xlim([121, 151])
+        elif val == 6:
+            plt.xlim([152, 181])
+        elif val == 7:
+            plt.xlim([182, 212])
+        elif val == 8:
+            plt.xlim([213, 243])
+        elif val == 9:
+            plt.xlim([244, 273])
+        elif val == 10:
+            plt.xlim([274, 304])
+        elif val == 11:
+            plt.xlim([305, 334])
+        elif val == 12:
+            plt.xlim([335, 365])
+
+    plt.subplot(111)
+    smin.on_changed(update)
+
     plt.show()
 
 
